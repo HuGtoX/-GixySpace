de2abf6ed15858506df5d74e2231e173667ef655
Gixy <1070856116@qq.com>
2025-04-18 20:29:58 +0800

    feat(frontend): 配置 antd 主题
    - 在 main.tsx 中添加 ConfigProvider 组件
    - 设置主题的 colorBgContainer 属性为 #fff

--- apps/frontend/easy-tools/src/main.tsx
+++ apps/frontend/easy-tools/src/main.tsx
@@ -2,10 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { ConfigProvider } from "antd";
+
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token，影响范围大
+          // 派生变量，影响范围小
+          colorBgContainer: "#fff",
+        },
+      }}>
+      <RouterProvider router={router} />
+    </ConfigProvider>
   </StrictMode>
 );
